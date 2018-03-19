746d45c9937dc346fbeacf53f41a2b66f0c246c2
longzhiwuing <czy3332842@gmail.com>
2018-03-19 21:02:34 +0800

    add webservice demo

---
+++ src/main/java/com/lzwing/testcode/webservice/InvokeDemo.java
@@ -0,0 +1,18 @@
+package com.lzwing.testcode.webservice;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: chenzhongyong@cecdat.com
+ * Date: 2018/3/19
+ * Time: 20:50
+ */
+public class InvokeDemo {
+    public static void main(String[] args) {
+        /*PhoneManager phoneManager = new PhoneManager();
+        PhoneService phoneService = phoneManager.getPhoneServicePort();
+        Phone android = phoneService.getMobileInfo("android");
+        System.out.println(android.getName()+":"+android.getOwner()+":"+android.getTotal());*/
+
+    }
+}
---
+++ src/main/java/com/lzwing/testcode/webservice/Phone.java
@@ -0,0 +1,11 @@
+package com.lzwing.testcode.webservice;
+
+import lombok.Data;
+
+@Data
+public class Phone {
+	private String name;//操作系统名
+	private String owner;//拥有者 
+	private int total;//市场占有率
+
+}
