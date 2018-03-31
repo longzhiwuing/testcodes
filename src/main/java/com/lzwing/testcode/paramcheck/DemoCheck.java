dbef07f8de85bdd49ebb160d980ad9708f47f15d
longzhiwuing <czy3332842@gmail.com>
2018-03-31 10:49:36 +0800

    add param check examples

---
+++ src/main/java/com/lzwing/testcode/paramcheck/DemoCheck.java
@@ -0,0 +1,44 @@
+package com.lzwing.testcode.paramcheck;
+
+import lombok.NonNull;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.Assert;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: chenzhongyong@cecdat.com
+ * Date: 2018/3/31
+ * Time: 10:25
+ */
+public class DemoCheck {
+    public static void main(String[] args) {
+        String source = null;
+//        System.out.println(lowerFirstLetter(source));
+
+        //lombok
+        System.out.println(lowerFirstLetter4Lombok(source));
+    }
+
+    private static String lowerFirstLetter4Lombok(@NonNull String source) {
+        return lowerFirstLetter(source);
+    }
+
+    private static String lowerFirstLetter(String source) {
+        //guava
+        checkArgument(StringUtils.isNotEmpty(source), "source should not null");
+
+
+        //spring utils
+        Assert.isTrue(StringUtils.isNotEmpty(source), "source should not null");
+
+        char[] chars = source.toCharArray();
+
+        chars[0] = (char) (chars[0] + 32);
+
+        return new String(chars);
+
+    }
+}
