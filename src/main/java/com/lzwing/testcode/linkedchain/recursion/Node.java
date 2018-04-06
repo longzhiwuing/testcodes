99b6b217ec1ae7b0385c9fde60803b71193ae2ec
longzhiwuing <czy3332842@gmail.com>
2018-04-06 18:37:34 +0800

    add recursion nodelist demo

---
+++ src/main/java/com/lzwing/testcode/linkedchain/recursion/Node.java
@@ -0,0 +1,18 @@
+package com.lzwing.testcode.linkedchain.recursion;
+
+import lombok.Data;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: chenzhongyong@cecdat.com
+ * Date: 2018/4/6
+ * Time: 17:27
+ */
+@Data
+public class Node<T> {
+
+    private final T value;
+
+    private Node next;
+}
