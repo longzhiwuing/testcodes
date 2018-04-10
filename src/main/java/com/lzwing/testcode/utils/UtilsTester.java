9dd80e3efafc02344c788a6455e355b1518404fd
longzhiwuing <czy3332842@gmail.com>
2018-04-10 11:03:01 +0800

    add utils demo

---
+++ src/main/java/com/lzwing/testcode/utils/UtilsTester.java
@@ -0,0 +1,21 @@
+package com.lzwing.testcode.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: chenzhongyong@cecdat.com
+ * Date: 2018/4/10
+ * Time: 10:40
+ */
+public class UtilsTester {
+    public static void main(String[] args) {
+
+
+//       http://www.hutool.cn/
+
+
+//        spring 相关utils
+//        WebApplicationContextUtils
+//        WebUtils
+    }
+}
