9bfe84703324d82bb0018a238ac1b01980bb8fa9
chenzhongyong <chenzhongyong@koolearn-inc.com>
2019-01-17 17:38:41 +0800

    添加mybatis plus

---
+++ src/main/java/com/lzwing/testcode/mybatisplus/entity/User.java
@@ -0,0 +1,17 @@
+package com.lzwing.testcode.mybatisplus.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@Data
+@TableName("user")
+public class User {
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+}
---
+++ src/main/java/com/lzwing/testcode/mybatisplus/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.lzwing.testcode.mybatisplus.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lzwing.testcode.mybatisplus.entity.User;
+
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
