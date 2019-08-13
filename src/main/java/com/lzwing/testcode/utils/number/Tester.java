33f10be1a149edf98760150b40a60eab1822f63c
chenzhongyong <chenzhongyong@koolearn-inc.com>
2019-08-13 22:33:52 +0800

    feat: 添加numberutils工具类

---
+++ src/main/java/com/lzwing/testcode/utils/number/Tester.java
@@ -0,0 +1,18 @@
+package com.lzwing.testcode.utils.number;
+
+import org.apache.commons.lang.math.NumberUtils;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: chenzhongyong
+ * Date: 2019/8/13
+ * Time: 22:31
+ */
+public class Tester {
+    public static void main(String[] args) {
+        int val = NumberUtils.toInt("1234");
+
+        System.out.println(val);
+    }
+}
