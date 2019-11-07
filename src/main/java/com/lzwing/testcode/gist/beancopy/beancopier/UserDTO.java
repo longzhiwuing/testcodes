835abfa44eb7c8503ecedc670dd67ea216a3868c
chenzhongyong <chenzhongyong@koolearn-inc.com>
2019-11-07 11:35:00 +0800

    feature: 添加beancopy例子

---
+++ src/main/java/com/lzwing/testcode/gist/beancopy/beancopier/UserDTO.java
@@ -0,0 +1,9 @@
+package com.lzwing.testcode.gist.beancopy.beancopier;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private int id;
+    private String userName;
+}
---
+++ src/main/java/com/lzwing/testcode/gist/beancopy/mapstruct/CarDto.java
@@ -0,0 +1,13 @@
+package com.lzwing.testcode.gist.beancopy.mapstruct;
+
+import lombok.Data;
+
+@Data
+public class CarDto {
+ 
+    private String make;
+    private int seatCount;
+    private String type;
+
+
+}
